55167ecfdbc9444d945862a547cfa8f1887db0e9
Miguel Rodriguez <miguel9@illinois.edu>
2023-07-30 16:14:24 -0700

    Add descriptions to all scenes

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ async function init() {
         // Title
         d3.select("#title")
             .html("Gasoline Prices");
+
+        displayBaseGas();
     })
     .catch(function(err) { console.log(err); });
 
@@ -135,6 +137,7 @@ function plotGraph(data, yMax, percent="") {
             .attr("cy", function(d) { return yScale(d.Price); } )
             .attr("r", 4)
                 .style("fill", "#e3de56")
+                .style('cursor', 'help')
     // Add tooltips
         .on('mouseover',
             function(d) {
@@ -174,16 +177,20 @@ function gasPrices() {
     d3.select("#title")
         .html("Gasoline Prices");
 
+    displayBaseGas();
+
     console.log("gasPrices clicked");
 }
 
-function barrelPrices() {
+function crudePrices() {
     state = States.Crude1;
     plotGraph(crudeData, crudeMax);
 
     // Title
     d3.select("#title")
         .html("Crude Oil Prices");
+
+    displayBaseCrude();
 
     console.log("barrelPrices clicked");
 }
@@ -195,6 +202,8 @@ function inflation() {
     // Title
     d3.select("#title")
         .html("Inflation Percentage");
+
+    displayBaseInflation();
 
     console.log("inflation clicked");
 }
@@ -209,65 +218,77 @@ function next() {
         case States.Gas1:
             annotate("U.S. Oil Imports Hit Two-Decade Low",135,126,"green");
             state = States.Gas2;
+            displayState1();
             break;
         case States.Gas2:
             annotate("U.S. Oil Imports Hit Two-Decade Low",135,126);
             annotate("Paris Agreement",475,218,"green");
             state = States.Gas3;
+            displayState2();
             break;
         case States.Gas3:
             annotate("U.S. Oil Imports Hit Two-Decade Low",135,126);
             annotate("Paris Agreement",475,218);
             annotate("COVID-19 Pandemic",885,212,"green");
             state = States.Gas4;
+            displayState3();
             break;
         case States.Gas4:
             annotate("U.S. Oil Imports Hit Two-Decade Low",135,126);
             annotate("Paris Agreement",475,218);
             annotate("COVID-19 Pandemic",885,212);
             annotate("Russia's War With Ukraine",1135,53,"green",-50,-70);
+            displayState4();
             break
         case States.Crude1:
             annotate("U.S. Oil Imports Hit Two-Decade Low",135,62,"green");
             state = States.Crude2;
+            displayState1();
             break;
         case States.Crude2:
             annotate("U.S. Oil Imports Hit Two-Decade Low",135,62);
             annotate("Paris Agreement",475,240,"green");
             state = States.Crude3;
+            displayState2();
             break;
         case States.Crude3:
             annotate("U.S. Oil Imports Hit Two-Decade Low",135,62);
             annotate("Paris Agreement",475,240);
             annotate("COVID-19 Pandemic",885,293,"green");
             state = States.Crude4;
+            displayState3();
             break;
         case States.Crude4:
             annotate("U.S. Oil Imports Hit Two-Decade Low",135,62);
             annotate("Paris Agreement",475,240);
             annotate("COVID-19 Pandemic",885,293);
             annotate("Russia's War With Ukraine",1135,34,"green",-50,-70);
+            displayState4();
             break;
         case States.Inflation1:
             annotate("U.S. Oil Imports Hit Two-Decade Low",135,299,"green",-50,-70);
             state = States.Inflation2;
+            displayState1();
             break;
         case States.Inflation2:
             annotate("U.S. Oil Imports Hit Two-Decade Low",135,299,"gray",-50,-70);
             annotate("Paris Agreement",475,271,"green",-50,-70);
             state = States.Inflation3;
+            displayState2();
             break;
         case States.Inflation3:
             annotate("U.S. Oil Imports Hit Two-Decade Low",135,299,"gray",-50,-70);
             annotate("Paris Agreement",475,271,"gray",-50,-70);
             annotate("COVID-19 Pandemic",885,271,"green",10,-70);
             state = States.Inflation4;
+            displayState3();
             break;
         case States.Inflation4:
             annotate("U.S. Oil Imports Hit Two-Decade Low",135,299,"gray",-50,-70);
             annotate("Paris Agreement",475,271,"gray",-50,-70);
             annotate("COVID-19 Pandemic",885,271,"gray",10,-70);
             annotate("Russia's War With Ukraine",1135,28,"green",-10,-20);
+            displayState4();
             break;
     }
     console.log(state);
@@ -310,4 +331,90 @@ function annotate(title,xval,yval,col="gray",dxval=-20,dyval=50) {
     svg.append("g")
         .attr("transform", `translate(${margin}, ${margin})`)
         .call(makeAnnotations );
+}
+
+function displayBaseGas() 
+{
+    d3.select("#description")
+        .html(
+            "Displayed here are average U.S. gasoline prices from January 2013 through June 2023. Hover over each scatter point to obtain the price for that specific month. \
+             Click on the \"Next Event\" button to view when global events occured and how average U.S. gasoline prices were affected by that event. Click on the \"Crude Oil Prices\"\
+             button to view data about crude oil or the \"Inflation\" button to view data about U.S. inflation.\
+            <br/>\
+            <br/> Data Source: <a href=\"https://data.bls.gov/timeseries/APU000074714\">https://data.bls.gov/timeseries/APU000074714</a>\
+            <br/> Event Source: <a href=\"https://www.cfr.org/timeline/oil-dependence-and-us-foreign-policy\">https://www.cfr.org/timeline/oil-dependence-and-us-foreign-policy</a>"
+        );
+}
+
+function displayBaseCrude() 
+{
+    d3.select("#description")
+        .html(
+            "Displayed here are average crude oil prices per barrel from January 2013 through June 2023. Hover over each scatter point to obtain the price for that specific month. \
+             Click on the \"Next Event\" button to view when global events occured and how average crude oil prices were affected by that event. Click on the \"Gas Prices\"\
+             button to view data about U.S. gasoline or the \"Inflation\" button to view data about U.S. inflation.\
+            <br/>\
+            <br/> Data Source: <a href=\"https://www.eia.gov/dnav/pet/hist/LeafHandler.ashx?n=PET&s=RWTC&f=M\">https://www.eia.gov/dnav/pet/hist/LeafHandler.ashx?n=PET&s=RWTC&f=M</a>\
+            <br/> Event Source: <a href=\"https://www.cfr.org/timeline/oil-dependence-and-us-foreign-policy\">https://www.cfr.org/timeline/oil-dependence-and-us-foreign-policy</a>"
+        );
+}
+
+function displayBaseInflation() 
+{
+    d3.select("#description")
+        .html(
+            "Displayed here are average U.S. inflation rates for each month from January 2013 through June 2023. Hover over each scatter point to obtain the percentage for that specific month. \
+             Click on the \"Next Event\" button to view when global events occured and see that there is not much correlation until the Ukraine-Russian war. Click on the \"Gas Prices\"\
+             button to view data about U.S. gasoline or the \"Crude Oil Prices\" button to view data about crude oil.\
+            <br/>\
+            <br/> Data Source: <a href=\"https://data.bls.gov/timeseries/CUUR0000SA0L1E?output_view=pct_12mths\">https://data.bls.gov/timeseries/CUUR0000SA0L1E?output_view=pct_12mths</a>\
+            <br/> Event Source: <a href=\"https://www.cfr.org/timeline/oil-dependence-and-us-foreign-policy\">https://www.cfr.org/timeline/oil-dependence-and-us-foreign-policy</a>"
+        );
+}
+
+function displayState1() 
+{
+    d3.select("#description")
+        .html(
+            "Imports of crude oil and petroleum products fall to less than 260,000 barrels per day, the lowest in almost two decades, according to the U.S. Energy Information Administration. \
+            The reduced reliance on foreign oil is the result of both declining demand and a domestic energy revolution which, through the combination of hydraulic fracturing and horizontal \
+            drilling, unlocked vast reserves of \"tight oil\" in shale rock formations. Tight oil production surges from less than one million barrels a day in 2010 to over four million \
+            barrels a day by December 2015, exceeding the individual production of every OPEC member except Saudi Arabia."
+        );
+}
+
+function displayState2() 
+{
+    d3.select("#description")
+        .html(
+            "The Paris Agreement—signed by more than 190 countries, including the United States—enters into force. The most ambitious climate accord to date, the agreement requires all parties \
+            to set targets to reduce greenhouse gas emissions, with the goal of arresting the rise in the average global temperature. Countries also agree to aim for net-zero carbon emissions \
+            by mid-century. The United States pledges to cut its emissions by more than 25 percent from 2005 levels by 2025, a move that requires transitioning away from fossil fuels, including oil. \
+            Although the accord does not include enforcement mechanisms, there are periodic performance reviews meant to encourage countries to adopt more ambitious targets."
+        );
+}
+
+function displayState3() 
+{
+    d3.select("#description")
+        .html(
+            "The world is rocked by the emergence of a new coronavirus disease, COVID-19, that quickly becomes a global pandemic. Response measures, including stay-at-home orders, trigger a sharp \
+            drop in the demand for oil. Falling oil prices create a rift within OPEC and lead to a price war between Saudi Arabia and Russia, with Riyadh ramping up production to further slash prices \
+            in an effort to force Moscow to the table. Oil prices hit rock bottom; in April, a major benchmark price for U.S. crude oil briefly falls below zero for the first time in history. \
+            After Whiting Petroleum Corporation, a major U.S. producer, declares bankruptcy, President Trump attempts to broker an OPEC deal to prevent further damage to the U.S. industry. \
+            After Trump intervenes, OPEC and Russia agree to curb production and raise prices."
+        );
+}
+
+function displayState4() 
+{
+    d3.select("#description")
+        .html(
+            "Russia's invasion of Ukraine causes turmoil in global oil markets. Biden blocks U.S. imports of oil from Russia, and Western sanctions cause energy companies to withdraw from the country. \
+            Oil prices, already rising in the wake of the pandemic, surge to their highest level since 2008. In response to near-record gasoline prices, U.S. lawmakers on both sides of the aisle call \
+            for boosting domestic oil production, though some in Congress urge a quicker transition to renewable energy. The United States and other members of the International Energy Agency announce \
+            plans to collectively release sixty million barrels of oil from strategic reserves. Meanwhile, the Biden administration considers smoothing rocky relations with Iran, Saudi Arabia, and \
+            Venezuela in the hope that those countries will supply more oil. Senior U.S. officials travel to Caracas for the first time since 2019, and the Biden administration pushes to finalize \
+            negotiations on reviving the 2015 Iran nuclear agreement and lifting U.S. sanctions on Tehran."
+        );
 }
